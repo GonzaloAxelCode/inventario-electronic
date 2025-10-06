415054173a4052c618634e556506da1ec5b732db
GonzaloAxelCode <gonzaloaxelcode@gmail.com>
2025-10-05 22:48:31 -0500

    probando con datos reales con vendor

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: true,
-	URL_BASE2: 'https://backend-accesories.onrender.com',
-	URL_BASE: "https://backend-accesories-production.up.railway.app",
+	URL_BASE: 'https://backend-accesories.onrender.com',
+	URL_BASE2: "https://backend-accesories-production.up.railway.app",
 	URL_BASE_FRONT: 'https://inventario-web-axel-drab.vercel.app',
 	API_VERSION: '1.0',
 };
