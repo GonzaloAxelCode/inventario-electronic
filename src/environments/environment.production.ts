1316e15f9b9805d3cd710809186b870c5b38bc8a
GonzaloAxelCode <gonzaloaxelcode@gmail.com>
2025-10-10 20:29:45 -0500

    Agregando dominios

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,6 @@
 export const environment = {
-	production: true,
+	production: false,
 	URL_BASE: 'http://174.138.55.7',
-	URL_BASE2: "https://backend-accesories-production.up.railway.app",
 	URL_BASE_FRONT: 'https://inventario-web-axel-drab.vercel.app',
 	API_VERSION: '1.0',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-	production: true,
-	URL_BASE: 'http://localhost:8000',
+	production: false,
+	URL_BASE: 'http://174.138.55.7',
 	URL_BASE_FRONT: 'http://localhost:4200',
 	API_VERSION: '1.0',
 };
