f8cc5d1373f63c4ad69f7f9e9360992cd964378a
GonzaloAxelCode <gonzaloaxelcode@gmail.com>
2025-10-10 23:01:53 -0500

    Agregando with credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: false,
-	URL_BASE: 'http://inventarioaxel.duckdns.org',
+	URL_BASE: 'https://inventarioaxel.duckdns.org',
 	URL_BASE_FRONT: 'http://localhost:4200',
 	API_VERSION: '1.0',
 };
